49554faaa1e80ecc8f3707a80f7e08781b63ba34
Shannon Millar <shanenak@gmail.com>
2023-10-22 21:32:47 -0700

    Add users and tweets

---
+++ backend/app.js
@@ -0,0 +1,45 @@
+// backend/app.js
+
+const express = require("express");
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const cors = require('cors');
+const csurf = require('csurf');
+const { isProduction } = require('./config/keys');
+
+const usersRouter = require('./routes/api/users');
+const tweetsRouter = require('./routes/api/tweets');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Security Middleware
+if (!isProduction) {
+  // Enable CORS only in development because React will be on the React
+  // development server (http://localhost:3000). (In production, React files
+  // will be served statically on the Express server.)
+  app.use(cors());
+}
+
+// Set the _csrf token and create req.csrfToken method to generate a hashed
+// CSRF token
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true
+    }
+  })
+);
+
+// Attach Express routers
+app.use('/api/users', usersRouter);
+app.use('/api/tweets', tweetsRouter);
+
+module.exports = app;
